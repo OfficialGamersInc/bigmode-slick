f03abff33576b3dc40a4b142765de2e32e9e96b6
Chris <32988106+Noobot9k@users.noreply.github.com>
2026-02-04 17:44:13 -0700

    Add stamina support to ability handler.
    - hasn't been implemented into any abilities yet. They'll have to have it implemented on each.

--- GodotProject/Character/Abilities/AbilityHandler.gd
+++ GodotProject/Character/Abilities/AbilityHandler.gd
@@ -1,16 +1,19 @@
 extends Node3D
 class_name AbilityHandler
 
+## set to -1 to set to stamina_move_max when the game starts.
+@export var stamina_move_cur : float = -1
+@export var stamina_move_max : int = 3
+## set to -1 to set to stamina_attack_max when the game starts.
+@export var stamina_attack_cur : float = -1
+@export var stamina_attack_max : int = 3
+@export var stamina_regen : float = 0.1
+
 var abilities : Array[Ability]
 
 # input
 var look_dir : Vector3
 signal attack_requested
-
-func _ready() -> void:
-	# Can't cast Array[any] to Array[Ability] so `abilities=find_children()` doesn't work. :/
-	abilities = []
-	abilities.assign(find_children("*", "Ability", false, true))
 
 func get_char_control() -> CharacterController:
 	return get_parent()
@@ -25,8 +28,31 @@ func set_look_dir(new_look : Vector3):
 	look_dir = new_look
 
 func request_attack():
+	# I think this should be done in Ability_Slash
+	#if not try_use_stamina_attack(): return
 	attack_requested.emit()
 
+func try_use_stamina_move() -> bool:
+	if stamina_move_cur < 1: return false
+	stamina_move_cur -= 1
+	return true
+
+func try_use_stamina_attack() -> bool:
+	if stamina_attack_cur < 1: return false
+	stamina_attack_cur -= 1
+	return true
+
+func _ready() -> void:
+	if stamina_move_cur < 0: stamina_move_cur = stamina_move_max
+	if stamina_attack_cur < 0: stamina_attack_cur = stamina_attack_max
+	
+	# Can't cast Array[any] to Array[Ability] so `abilities=find_children()` doesn't work. :/
+	abilities = []
+	abilities.assign(find_children("*", "Ability", false, true))
+
+func _process(delta: float) -> void:
+	stamina_attack_cur += stamina_regen * delta
+	stamina_move_cur += stamina_regen * delta
 
 
 
