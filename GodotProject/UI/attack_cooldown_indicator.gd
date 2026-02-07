1ed95e43ab114c9c3e4cf61345a9f316b12ed42d
SwPGameDev <swpgamedev@gmail.com>
2026-02-06 17:25:09 -0700

    Indicator if out of attack stam

--- GodotProject/UI/attack_cooldown_indicator.gd
+++ GodotProject/UI/attack_cooldown_indicator.gd
@@ -1,23 +1,35 @@
 extends Node
 
 @export var player : CharacterController
-
 @export var attack_cooldown_bar : ProgressBar
+@export var grayout : ColorRect
 
+var ability_handler : AbilityHandler
 var slash_ability : Ability_Slash
+
+enum CooldownBarStates {Grayed, Highlighted, Crit}
+var AbleAttackState : CooldownBarStates
 
 func _ready() -> void:
 	if player != null :
+		ability_handler = NodeLib.FindChildOfCustomClass(player, AbilityHandler, true, true)
 		slash_ability = NodeLib.FindChildOfCustomClass(player, Ability_Slash, true, true)
-		
-		
 		
 		attack_cooldown_bar.value = slash_ability.attackTimer / slash_ability.attackCooldown
 	
 
-func _process(delta: float) -> void:
-	print(str(slash_ability.canAttack))
-	#print(str(slash_ability.attackTimer / slash_ability.attackCooldown))
+func _process(_delta: float) -> void:
+	if ability_handler.stamina_attack_cur > 1 and AbleAttackState != CooldownBarStates.Highlighted :
+		AbleAttackState = CooldownBarStates.Highlighted
+		grayout.visible = false
+		
+	elif ability_handler.stamina_attack_cur < 1 and AbleAttackState != CooldownBarStates.Grayed:
+		AbleAttackState = CooldownBarStates.Grayed
+		grayout.visible = true
+		
 	
-	attack_cooldown_bar.value = slash_ability.attackTimer / slash_ability.attackCooldown
+	if slash_ability.canAttack :
+		attack_cooldown_bar.value = 1
+	else :
+		attack_cooldown_bar.value = slash_ability.attackTimer / slash_ability.attackCooldown
 	
